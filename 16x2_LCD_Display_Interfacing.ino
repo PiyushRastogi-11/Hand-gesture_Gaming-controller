f5d16b79c1d3ce452eacca3da0cd30075fb53b20
PiyushRastogi-11 <143533872+PiyushRastogi-11@users.noreply.github.com>
2025-04-29 23:28:24 +0530

    Add files via upload

---
+++ 16x2_LCD_Display_Interfacing.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>;
+#include <LiquidCrystal_I2C.h>;
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+
+}
+
+void loop() {
+  lcd.setcursor(0,0);
+  lcd.print("Hello world");
+  delay(5000);
+
+}
